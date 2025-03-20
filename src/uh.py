ce97acba7ba832dead0848356d22b76ef03d4d71
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-03-20 13:43:09 +0000

    Update uh.py

--- src/uh.py
+++ src/uh.py
@@ -61,7 +61,6 @@ V=1
 
 ## load jules results, change accordingly
 #jules = xr.open_dataset(home+"/hydrodata/jules_runs/coupled/runoff_scaled_and_partitioned.nc",decode_coords="all")
-
 jules = xr.open_dataset(os.path.join(INPUT_DIRECTORY,NBS, INPUT_FILENAME),decode_coords="all") # MODIFY THIS DEPENDING ON HOW WE LOOP FOR BARIABLES
 
 ## Change depending shapefile
@@ -73,17 +72,29 @@ q_deep=jules.q_deep.rio.set_spatial_dims(x_dim="lon",y_dim="lat",inplace=True)
 q_deep.rio.set_spatial_dims(x_dim="lon",y_dim="lat",inplace=True)
 q_deep.rio.write_crs("epsg:4326",inplace=True)
 
+grid_area = xr.open_dataset(os.path.join(INPUT_DIRECTORY,NBS,"mosart_new.nc"))['area']
+grid_area = grid_area.expand_dims("time")
+grid_area['lat']=q_deep['lat']
+grid_area['lon']=q_deep['lon']
+deep_total_ts = np.multiply(q_deep, grid_area[0,:,:]).sum(dim=("lat","lon"))/1000
+#deep_total_ts = q_deep.sum(dim=["lat","lon"])
+#deep_total_ts = deep_total_ts*83424939/1000 # think we should multiply by each gridcell area in the grid instead...
+
 q_deep_km105=q_deep.rio.clip(catchment_km105.geometry.apply(mapping),catchment_km105.crs,drop=False,all_touched=True)
-deep_km105_ts = q_deep_km105.sum(dim=["lat","lon"])
-deep_km105_ts=deep_km105_ts*83424939/1000 # multiplying by mean pixel area for 48x48 grid
+deep_km105_ts = np.multiply(q_deep_km105, grid_area[0,:,:]).sum(dim=("lat","lon"))/1000
+#deep_km105_ts = q_deep_km105.sum(dim=["lat","lon"])
+#deep_km105_ts=deep_km105_ts*83424939/1000 # multiplying by mean pixel area for 48x48 grid
 
 q_deep_pisac=q_deep.rio.clip(catchment_pisac.geometry.apply(mapping),catchment_pisac.crs,drop=False,all_touched=True)
-deep_pisac_ts = q_deep_pisac.sum(dim=["lat","lon"])
-deep_pisac_ts=deep_pisac_ts*83424939/1000 # multiplying by mean pixel area for 48x48 grid
+deep_pisac_ts = np.multiply(q_deep_pisac, grid_area[0,:,:]).sum(dim=("lat","lon"))/1000
+#deep_pisac_ts = q_deep_pisac.sum(dim=["lat","lon"])
+#deep_pisac_ts=deep_pisac_ts*83424939/1000 # multiplying by mean pixel area for 48x48 grid
 
 
 
 ## convolve timeseries
+[deeper_mod_total,uh] = unit_hydro(V,k0_km105,n0_km105,np.arange(deep_total_ts.size),deep_total_ts) # Assumption here is we use the uh parameters from km105 derived by Jose to be applied for the entire catchment so that we can obtain a timeseries of deep contributions for the full catchment
+
 [deeper_mod_km105,uh]= unit_hydro(V,k0_km105,n0_km105,np.arange(deep_km105_ts.size),deep_km105_ts)
 
 [deeper_mod_pisac,uh]= unit_hydro(V,k0_pisac,n0_pisac,np.arange(deep_pisac_ts.size),deep_pisac_ts)
@@ -91,9 +102,11 @@ deep_pisac_ts=deep_pisac_ts*83424939/1000 # multiplying by mean pixel area for 4
 ## Write deeper baseflows to csv files
 t = np.arange(datetime(2000,1,1), datetime(2100,1,1), timedelta(days=1)).astype(datetime)
 
+df_total = pd.DataFrame({"time" : t, "deeper_baseflow" : deeper_mod_total})
+df_total.to_csv(os.path.join(OUTPUT_DIRECTORY, NBS, SC+"_"+NBS+"deeper_total.csv"), index=False)
+
 df_km105 = pd.DataFrame({"time" : t, "deeper_baseflow" : deeper_mod_km105})
 df_km105.to_csv(os.path.join(OUTPUT_DIRECTORY,NBS,SC+"_"+NBS,"deeper_km105.csv"), index=False) # MODIFY PATHS HERE AS NEEDED
 
 df_pisac = pd.DataFrame({"time" : t, "deeper_baseflow" : deeper_mod_pisac})
 df_pisac.to_csv(os.path.join(OUTPUT_DIRECTORY,NBS,SC+"_"+NBS,"deeper_pisac.csv"), index=False) # MODIFY PATHS HERE AS NEEDED
-
