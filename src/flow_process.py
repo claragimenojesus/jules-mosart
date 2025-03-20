04e7a3d2428b871d3012033643a9844ce7404366
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-03-20 13:43:59 +0000

    Update flow_process.py

--- src/flow_process.py
+++ src/flow_process.py
@@ -49,33 +49,46 @@ for scenario in SCENARIOS:
     file['lat'] = GRID_FILE['lat']
     file['lon'] = GRID_FILE['lon']
 
-    # changed to start from 2002 to give warm up period to deep flow contribution
-    hist_qg_m = file['melt'].sel(time=slice("2002-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
+    #hist_qg_m = file['melt'].sel(time=slice("2000-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
+    hist_qg_m = file['melt'].sel(time=slice("2002-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean() # Selecting 2002 to 2018 because uh deep has 2 year memory and needs to spinup
     hist_qg_m = np.multiply(hist_qg_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
+    #hist_qs_m = file['surf_roff'].sel(time=slice("2000-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qs_m = file['surf_roff'].sel(time=slice("2002-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qs_m = np.multiply(hist_qs_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
+    #hist_qss_m = file['sub_surf_roff'].sel(time=slice("2000-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qss_m = file['sub_surf_roff'].sel(time=slice("2002-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qss_m = np.multiply(hist_qss_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
+    #hist_qt_m = (file['surf_roff']+file['sub_surf_roff']).sel(time=slice("2000-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qt_m = (file['surf_roff']+file['sub_surf_roff']).sel(time=slice("2002-01-01","2018-12-31")).resample(time="M").mean().groupby("time.month").mean()
     hist_qt_m = np.multiply(hist_qt_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
-    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
-    km105['time'] = pd.to_datetime(km105['time'])
-    km105 = km105.set_index("time")
-    hist_km105 = km105.loc['2002-01-01':'2018-12-31']
-    hist_km105 = hist_km105.reset_index()
-    hist_km105 = hist_km105.groupby(hist_km105['time'].dt.month).mean()
-    hist_flow_m = pd.DataFrame({'month':months, 'flow':hist_qt_m.values}).set_index("month")
+#    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
+#    km105['time'] = pd.to_datetime(km105['time'])
+#    km105 = km105.set_index("time")
+    #hist_km105 = km105.loc['2000-01-01':'2018-12-31']
+#    hist_km105 = km105.loc['2002-01-01':'2018-12-31']
+#    hist_km105 = hist_km105.reset_index()
+#    hist_km105 = hist_km105.groupby(hist_km105['time'].dt.month).mean()
+#    hist_flow_m = pd.DataFrame({'month':months, 'flow':hist_qt_m.values}).set_index("month")
+
+    # Instead since we take the contributing flows from entire catchment, let's take deep contribution derived for total catchment
+    deep = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_total.csv"))
+    deep['time'] = pd.to_datetime(deep['time'])
+    deep = deep.set_index("time")
+    hist_deep = deep.loc['2002-01-01':'2018-12-31']
+    hist_deep = hist_deep.reset_index()
+    hist_deep = hist_deep.groupby(hist_deep['time'].dt.month).mean()
+    hist_flow_m = pd.DataFrame){'month':months, 'flow':hist_qt_m.values}).set_index("month")
 
     hist_qgm_45_df[scenario] = hist_qg_m.values
-    hist_qtm_45_df[scenario] = hist_flow_m['flow'] + hist_km105['deeper_baseflow'] # assumption of no monthly delay for surface and shallow subsurface
+    hist_qtm_45_df[scenario] = hist_flow_m['flow'] + hist_deep['deeper_baseflow'] # assumption of no monthly delay for surface and shallow subsurface
     #hist_qngm_45_df[scenario] = hist_qtm_45_df[scenario] - hist_qgm_45_df[scenario]
     hist_qsm_45_df[scenario] = hist_qs_m.values-hist_qg_m.values
     hist_qssm_45_df[scenario] = hist_qss_m.values
-    hist_qdm_45_df[scenario] = hist_km105['deeper_baseflow']
+    hist_qdm_45_df[scenario] = hist_deep['deeper_baseflow']
 
 
     fut_qg_m = file['melt'].sel(time=slice("2080-01-01","2100-12-31")).resample(time="M").mean().groupby("time.month").mean()
@@ -90,20 +103,28 @@ for scenario in SCENARIOS:
     fut_qss_m = file['sub_surf_roff'].sel(time=slice("2080-01-01","2100-12-31")).resample(time="M").mean().groupby("time.month").mean()
     fut_qss_m = np.multiply(fut_qss_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
-    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
-    km105['time'] = pd.to_datetime(km105['time'])
-    km105 = km105.set_index("time")
-    fut_km105 = km105.loc['2080-01-01':'2100-12-31']
-    fut_km105 = fut_km105.reset_index()
-    fut_km105 = fut_km105.groupby(fut_km105['time'].dt.month).mean()
+#    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
+#    km105['time'] = pd.to_datetime(km105['time'])
+#    km105 = km105.set_index("time")
+#    fut_km105 = km105.loc['2080-01-01':'2100-12-31']
+#    fut_km105 = fut_km105.reset_index()
+#    fut_km105 = fut_km105.groupby(fut_km105['time'].dt.month).mean()
+#    fut_flow_m = pd.DataFrame({'month':months, 'flow':fut_qt_m.values}).set_index("month")
+
+    deep = pd.read_csv(os.path.join(MOSART_OUTPUT,RC45,scenario,NBS,scenario+"_"+NBS,"deeper_total.csv"))
+    deep['time'] = pd.to_datetime(deep['time'])
+    deep = deep.set_index("time")
+    fut_deep = deep.loc['2080-01-01':'2100-12-31']
+    fut_deep = fut_deep.reset_index()
+    fut_deep = fut_deep.groupby(fut_deep['time'].dt.month).mean()
     fut_flow_m = pd.DataFrame({'month':months, 'flow':fut_qt_m.values}).set_index("month")
 
     fut_qgm_45_df[scenario] = fut_qg_m.values
-    fut_qtm_45_df[scenario] = fut_flow_m['flow'] + fut_km105['deeper_baseflow']
+    fut_qtm_45_df[scenario] = fut_flow_m['flow'] + fut_deep['deeper_baseflow']
     #fut_qngm_45_df[scenario] = fut_qtm_45_df[scenario] - fut_qgm_45_df[scenario]
     fut_qsm_45_df[scenario] = fut_qs_m.values-fut_qg_m.values
     fut_qssm_45_df[scenario] = fut_qss_m.values
-    fut_qdm_45_df[scenario] = fut_km105['deeper_baseflow']
+    fut_qdm_45_df[scenario] = fut_deep['deeper_baseflow']
 
     file = xr.open_dataset(os.path.join(INPUT_FILE, RC85, scenario, NBS,FILENAME))
     file['lat'] = GRID_FILE['lat']
@@ -121,20 +142,28 @@ for scenario in SCENARIOS:
     fut_qss_m = file['sub_surf_roff'].sel(time=slice("2080-01-01","2100-12-31")).resample(time="M").mean().groupby("time.month").mean()
     fut_qss_m = np.multiply(fut_qss_m,GRID_FILE[0,:,:]).sum(dim=("lat","lon"))/1000 # kg/m2/s*m2/1000=m3/s
 
-    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC85,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
-    km105['time'] = pd.to_datetime(km105['time'])
-    km105 = km105.set_index("time")
-    fut_km105 = km105.loc['2080-01-01':'2100-12-31']
-    fut_km105 = fut_km105.reset_index()
-    fut_km105 = fut_km105.groupby(fut_km105['time'].dt.month).mean()
+#    km105 = pd.read_csv(os.path.join(MOSART_OUTPUT,RC85,scenario,NBS,scenario+"_"+NBS,"deeper_km105.csv"))
+#    km105['time'] = pd.to_datetime(km105['time'])
+#    km105 = km105.set_index("time")
+#    fut_km105 = km105.loc['2080-01-01':'2100-12-31']
+#    fut_km105 = fut_km105.reset_index()
+#    fut_km105 = fut_km105.groupby(fut_km105['time'].dt.month).mean()
+#    fut_flow_m = pd.DataFrame({'month':months, 'flow':fut_qt_m.values}).set_index("month")
+
+    deep = pd.read_csv(os.path.join(MOSART_OUTPUT,RC85,scenario,NBS,scenario+"_"+NBS,"deeper_total.csv"))
+    deep['time'] = pd.to_datetime(deep['time'])
+    deep = deep.set_index("time")
+    fut_deep = deep.loc['2080-01-01':'2100-12-31']
+    fut_deep = fut_deep.reset_index()
+    fut_deep = fut_deep.groupby(fut_deep['time'].dt.month).mean()
     fut_flow_m = pd.DataFrame({'month':months, 'flow':fut_qt_m.values}).set_index("month")
 
     fut_qgm_85_df[scenario] = fut_qg_m.values
-    fut_qtm_85_df[scenario] = fut_flow_m['flow'] + fut_km105['deeper_baseflow']
+    fut_qtm_85_df[scenario] = fut_flow_m['flow'] + fut_deep['deeper_baseflow']
     #fut_qngm_85_df[scenario] = fut_qtm_85_df[scenario] - fut_qgm_85_df[scenario]
     fut_qsm_85_df[scenario] = fut_qs_m.values-fut_qg_m.values
     fut_qssm_85_df[scenario] = fut_qss_m.values
-    fut_qdm_85_df[scenario] = fut_km105['deeper_baseflow']
+    fut_qdm_85_df[scenario] = fut_deep['deeper_baseflow']
 
 
 fut_qgm_85_df.to_csv("/home/clara/NbS_paper/processed_flows/"+NBS+"/fut_qgm_85_df.csv")
