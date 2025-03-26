ee8c0ac5ce98384103355062fd36ac9def2a14bb
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-03-26 13:04:38 +0000

    Update amunas.py

--- src/amunas.py
+++ src/amunas.py
@@ -28,43 +28,36 @@ SC = str(os.environ['SCENARIO'])
 ## import jules runs (note this is 2000 to 2099)
 ## scenario rcp45_ACCESS1-0
 #jules = xr.open_dataset(home+"/hydrodata/jules_runs/coupled_for_nbs/rcp85_ACCESS1-3_coupled_jules_oggm_00_99.nc",decode_coords="all")
-jules = xr.open_dataset(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_coupled_jules_oggm_00_99.nc"), decode_coords="all")
+jules = xr.open_dataset(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_degradation_coupled_jules_oggm_00_99.nc"), decode_coords="all")
 
-# import high res dem
-dem = rxr.open_rasterio(home+'/sa_dem_3s.tif').isel(band=0).rio.clip_box(minx=-74,miny=-16,maxx=-70,maxy=-12)
-
-
-## regrid dem
-threshold = 4000
-regridder=xe.Regridder(dem,jules,"bilinear")
-dr_out=regridder((dem>threshold).astype(float),keep_attrs=True)
-
-
-### We will deviating water according to Ochoa Tocachi et al (2019)
-### i.e. the diversion operation rules is: will diverge whenever flow is greater than 4 l/s/km2 but no more than 39.9 l/s/km2. 10^6 l/s/km2 = kg/m2/s. So it is 4E-5 and 4E-6
-## where operator will limit the diversion for december to april
-## If else statement will shut off diversion if flow is too low
-## np.minimum function will limit the diversion to the minimum between the diversion (flow-minimum flow for supporting water use) and the channel capacity
-operator=jules.surf_roff.where(jules.surf_roff.time.dt.month.isin([1,2,3,4,12]))
-operator=operator.where(operator>4E-6)
-diversion=np.minimum(operator-4E-6,4E-5)
-diversion=diversion*dr_out
+#mask >4000masl
+mask=xr.open_dataset("/rds/general/user/cg2117/home/netcdf/mask_4000m.nc")['frac']
 
 ## load UH
 uh = pd.read_csv(home+'/unit_hydro.csv').fillna(0)
 uh["conv"]=uh["conv"]/uh["conv"].sum()
 
+area=xr.zeros_like(jules.surf_roff)
+ET0=xr.zeros_like(jules.surf_roff)
 
+div_ratio=100 # 100% diversion scenario
+
+## et area will vary linearly with the amount of water diverted
+et_ratio = 1.2E6/75 #75l/s equated to 1.2 km2 of greened area 
+
+## calculating diverted water
+diversion=np.multiply(jules.surf_roff,mask)
+diversion= div_ratio/100*diversion.where(diversion.time.dt.month.isin([1,2,3,4,12])) # kg/m2/s
+area= np.multiply(diversion,cell_area[0,...])*et_ratio # this is the greened area
 ## Correct surface flow
-a=jules.surf_roff-diversion.fillna(0)
-jules.surf_roff.values=a.values
+jules["surf_roff"]= jules.surf_roff-diversion.fillna(0)
 
 # Correct subsurface flow
 # Calculate ET
-operator_et=jules.fao_et0.where(diversion>0)
-et=np.minimum(operator_et*120000*8/15526711,diversion) ## note that we need to adjust 15526711 to cell size
+ET0=np.divide(np.multiply(area,jules.fao_et0), #kg/s
+              cell_area[0,...]) #kg/m2/s
 # Calculate infiltration
-infilt=diversion.fillna(0)-et.fillna(0)
+infilt=diversion.fillna(0)-ET0.fillna(0) ##infiltration is now in kg/m2/s
 ## unit hydrograph i,j wise for shallow subsurface flow
 shallow_asnp = infilt.values
 shallow_asnp = np.append(shallow_asnp,np.zeros((365,75,102)),axis=0)
@@ -74,9 +67,12 @@ for i in range(jules.sizes["lon"]):
         for t in range(jules.sizes["time"]):
             if shallow_asnp[t,j,i] > 0:
                 shallow_asnp1[t:t+uh["conv"].size,j,i]+= shallow_asnp[t,j,i]*uh["conv"].values
+                
+
+
 #replace flow
 jules.sub_surf_roff.values += shallow_asnp1[:jules.sizes["time"],...]
 jules.runoff.values = jules.surf_roff.values + jules.sub_surf_roff.values
-jules.esoil_gb.values += et.values
+jules.esoil_gb.values += ET0.values
 # Save netcdf
 jules.to_netcdf(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_"+"amunas_coupled_jules_oggm_00_99.nc"))
