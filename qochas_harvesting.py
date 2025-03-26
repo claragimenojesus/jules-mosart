1e7c4618bcab89eccf8f669608ec1202dcbc286c
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-03-26 17:16:37 +0000

    Update qochas_harvesting.py

--- qochas_harvesting.py
+++ qochas_harvesting.py
@@ -42,12 +42,6 @@ qochas_volumes=1E6
 vol_area=11652.64/13729.72
 vol_acc=202500/13729.72
 
-# read unit hydrograph for shallow reinfiltration
-uh = pd.read_csv(home+'/unit_hydro.csv').fillna(0)
-uh["conv"]=uh["conv"]/uh["conv"].sum()
-
-## hydraulic conductivity
-satcon = xr.open_dataset("/rds/general/user/cg2117/home/netcdf/jules_soil_props_2015_rosetta3_ESA_rahu_modified_v2.nc")['satcon']
 
 # cell area
 cell_area = xr.open_dataset("/rds/general/user/cg2117/home/netcdf/gridcell_area_rahu_v2.nc")['area']
@@ -69,9 +63,9 @@ qochas_acc = qochas_cap*vol_acc
 
 ### Zero arrays for various intermediate variables
 St=xr.zeros_like(jules.surf_roff) #Storage at time step t
-Qav = xr.zeros_like(satcon[0,...]) # storage volume + contributing runoff - losses (Et + drainage). or available storage
-R = xr.zeros_like(jules.surf_roff) # Potential recharge at time step t
-Qin = xr.zeros_like(satcon[0,...])
+Qav = xr.zeros_like(cell_area[0,...]) # storage volume + contributing runoff - losses (Et + drainage). or available storage
+#R = xr.zeros_like(jules.surf_roff) # Potential recharge at time step t
+Qin = xr.zeros_like(cell_area[0,...])
 ET = xr.zeros_like(jules.surf_roff) 
 
 
@@ -79,9 +73,7 @@ ET = xr.zeros_like(jules.surf_roff)
 # cell_area=15526711 #grid_area[0,j,i].values for grid area
 for t in range(jules.sizes["time"]-1):
     #### Calculate losses
-    # Recharge limited by the hydraulic conductivity or the available water
-    R[t,...]=np.minimum(86.4*satcon[0,...]*qochas_area,St[t,...])
-    St[t,...]=np.add(St[t,...],-R[t,...])
+    
     ## calculate ET
     ET[t,...]=np.minimum(np.abs(jules.fao_et0[t,...]) * qochas_area * 86.4,
                             St[t,...])
@@ -89,56 +81,20 @@ for t in range(jules.sizes["time"]-1):
     ## Calculate available storage
     Qav=np.add(qochas_cap,-St[t,...])
     # water that actually enters the qocha is limited by the storage capacity
-    # Qin = (xr.where(Qav>qochas_cap, qochas_cap-St[t,...], qochas_acc*jules.surf_roff[t,...]*86.4))
+
     # Calculate inflow. This is considering overflow, i.e. cannot flow more than available storage
     Qin= np.minimum(qochas_acc*jules.surf_roff[t,...]*86.4,Qav)
     ## correcting flow
     jules.surf_roff[t,...]= (jules.surf_roff[t,...]-(np.divide(Qin.values/86.4,cell_area[0,...])))
     #update storage
     St[t+1,...] = np.add(Qin,St[t,...])
-# add shallow subsurface delay because of re infiltration
-shallow_asnp = R.values
-shallow_asnp = np.append(shallow_asnp, np.zeros((365,75,102)),axis=0)
-shallow_asnp1 = np.zeros_like(shallow_asnp)
-for i in range(jules.sizes["lon"]):
-    for j in range(jules.sizes["lat"]):
-        for t in range(jules.sizes["time"]):
-            if shallow_asnp[t,j,i] > 0:
-                shallow_asnp1[t:t+uh["conv"].size,j,i]+= (shallow_asnp[t,j,i]*uh["conv"]).values
-#replace flow
-jules.sub_surf_roff.values += np.divide(shallow_asnp1[:jules.sizes["time"],...]/86.4,cell_area[0,...].values)
+
 jules.runoff.values = jules.surf_roff.values + jules.sub_surf_roff.values
+jules['ET_qocha']=ET
+jules['St_qocha']=St
 
-## accumulate surface
-surf_roff=jules.surf_roff.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean()
-surf_roff=np.multiply(surf_roff,cell_area[0,...]).sum(dim=["lat","lon"])
-surf_roff=surf_roff/1000
-   
-# Subsurface partitioning
-q_deep = xr.zeros_like(jules.sub_surf_roff)
-# calculate deep percolation. Hydraulic conductivity is multiplied by 1000 (m/s to mm/s )
-q_deep[...]=np.minimum(jules.sub_surf_roff,1000*hyd_cond0)
-## correct shallow subsurface flow
-jules.sub_surf_roff.values = np.add(jules.sub_surf_roff,-q_deep)
-sub_surf_roff=jules.sub_surf_roff.sel(time=jules.sub_surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean()
-sub_surf_roff=np.multiply(sub_surf_roff,cell_area[0,...]).sum(dim=["lat","lon"])
-sub_surf_roff=sub_surf_roff/1000
-## JULES gridded results as timeseries
-deep_ts = np.multiply(q_deep,cell_area[0,...]).sum(dim=["lat","lon"])
-deep_ts = deep_ts/1000
-deep_ts = deep_ts.values
-## convolve timeseries
-[deeper_mod,uh2]= unit_hydro(V,k0,n0,np.arange(deep_ts.size),deep_ts)
-pd_deep=pd.DataFrame({"deeper":deeper_mod},index=q_deep.time.values)["2080-01-01":]
-pd_deep=pd_deep.groupby(pd_deep.index.month).mean()
-total_flow=surf_roff.values+sub_surf_roff.values+pd_deep.to_numpy().flatten()
-qochas_scenarios_flow[counter,:]=total_flow
-available_storage[counter,:]=St.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
-et_month[counter,:]=ET.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
 
-    counter+=1
+#available_storage[counter,:]=St.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
+#et_month[counter,:]=ET.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
 
-    
-np.savetxt("scaling_qochas_imp_85.csv",qochas_scenarios_flow,delimiter=',')
-np.savetxt("scaling_qochas_sto_85.csv",available_storage,delimiter=',')
-np.savetxt("scaling_qochas_ET_85.csv",et_month,delimiter=',')
+
