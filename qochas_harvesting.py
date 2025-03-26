89dafba33958890c45506cfb2279a9222f9474f5
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-03-26 18:01:51 +0000

    Update qochas_harvesting.py

--- qochas_harvesting.py
+++ qochas_harvesting.py
@@ -25,7 +25,7 @@ SC = str(os.environ['SCENARIO'])
 
 ## jules output. change accordingly
 #jules = xr.open_dataset(home+"/hydrodata/jules_runs/coupled_for_nbs/rcp85_ACCESS1-3_coupled_jules_oggm_00_99.nc",decode_coords="all").sel(time=slice("2000-01-01", "2018-12-31"))
-jules = xr.open_dataset(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_coupled_jules_oggm_00_99.nc"), decode_coords="all")
+jules = xr.open_dataset(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_degradation_coupled_jules_oggm_00_99.nc"), decode_coords="all")
 
 #mask >4000masl
 mask=xr.open_dataset("/rds/general/user/cg2117/home/netcdf/mask_4000m.nc")['frac']
@@ -36,9 +36,9 @@ mask=xr.open_dataset("/rds/general/user/cg2117/home/netcdf/mask_4000m.nc")['frac
 # Storage to contribution area is 13729.72/202500 = 0.06558. The cell size is an average of 1.56E7 m2.
 # then the storage volume will be 1023030 M3, so 1E6 m3.
 # [1] 13729.72 ## storage volume m3
-# [1] 202500 ## contribution area m2 
+# [1] 202500 ## contribution area m2
 # [1] 11652.64 ## qocha area m2
-qochas_volumes=1E6
+st_max=1E6
 vol_area=11652.64/13729.72
 vol_acc=202500/13729.72
 
@@ -50,10 +50,6 @@ cell_area = xr.open_dataset("/rds/general/user/cg2117/home/netcdf/gridcell_area_
 masking=jules.surf_roff.sum(dim="time")
 masking=masking.where(masking>0)
 
-## start scenarios array
-qochas_scenarios_flow= np.zeros((qochas_volumes.size,12))
-available_storage=np.zeros_like(qochas_scenarios_flow)
-et_month=np.zeros_like(qochas_scenarios_flow)
 
 
 qochas_cap=st_max
@@ -66,18 +62,17 @@ St=xr.zeros_like(jules.surf_roff) #Storage at time step t
 Qav = xr.zeros_like(cell_area[0,...]) # storage volume + contributing runoff - losses (Et + drainage). or available storage
 #R = xr.zeros_like(jules.surf_roff) # Potential recharge at time step t
 Qin = xr.zeros_like(cell_area[0,...])
-ET = xr.zeros_like(jules.surf_roff) 
+ET = xr.zeros_like(jules.surf_roff)
 
 
 ### Qocha model
 # cell_area=15526711 #grid_area[0,j,i].values for grid area
 for t in range(jules.sizes["time"]-1):
     #### Calculate losses
-    
+
     ## calculate ET
-    ET[t,...]=np.minimum(np.abs(jules.fao_et0[t,...]) * qochas_area * 86.4,
-                            St[t,...])
-    St[t,...]=np.add(St[t,...],-ET)
+    ET[t,...]=np.minimum(np.abs(jules.fao_et0[t,...]) * qochas_area * 86.4,St[t,...])
+    St[t,...]=np.add(St[t,...],-ET[t,...])
     ## Calculate available storage
     Qav=np.add(qochas_cap,-St[t,...])
     # water that actually enters the qocha is limited by the storage capacity
@@ -92,6 +87,7 @@ for t in range(jules.sizes["time"]-1):
 jules.runoff.values = jules.surf_roff.values + jules.sub_surf_roff.values
 jules['ET_qocha']=ET
 jules['St_qocha']=St
+jules.drop_vars(["melt","perc","rain","area","precip","rainfall","snowfall","fao_et0","esoil_gb","ecan_gb","ei_gb","elake"])
 
 # Save netcdf
 jules.to_netcdf(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_"+"qochas_harvesting_coupled_jules_oggm_00_99.nc"))
@@ -99,5 +95,3 @@ jules.to_netcdf(os.path.join(SCENARIO_SH_OUTPUT, RC+"_"+SC+"_"+"qochas_harvestin
 
 #available_storage[counter,:]=St.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
 #et_month[counter,:]=ET.sel(time=jules.surf_roff.time.dt.year.isin(np.arange(2080,2101)),drop=True).resample(time="ME").mean().groupby("time.month").mean().mean(dim=["lat","lon"])
-
-
