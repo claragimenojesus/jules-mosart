869568353dc992bc8ef87ab7f954c5b6d0b82f4e
claragimenojesus <79370643+claragimenojesus@users.noreply.github.com>
2025-04-11 14:12:05 +0100

    Update full_job_run_HPC.sh

--- full_job_run_HPC.sh
+++ full_job_run_HPC.sh
@@ -22,55 +22,55 @@ export RC="rcp45"
 
 echo "This script will pre-process rcp85 scenarios for MOSART routing"
 echo "Regridding jules output"
-#./01_regrid_jules_output.sh $CONFIG_FILE
-#./01_regrid_jules_output.sh $CONFIG_FILE_METEO
+./01_regrid_jules_output.sh $CONFIG_FILE
+./01_regrid_jules_output.sh $CONFIG_FILE_METEO
 
 
 export SRC_DIR=$(pwd)/../src
 echo "Coupling with JULES-OGGM"
-#python ${SRC_DIR}/coupling_pt1.py
+python ${SRC_DIR}/coupling_pt1.py
 
 
 cd $SCENARIO_SH
-#cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_area_yyyy.nc glacier_area_yyyy.nc glacier_area_daily.nc
-#cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_perc_per_gridcell.nc glacier_perc_per_gridcell.nc glacier_perc_daily.nc
+cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_area_yyyy.nc glacier_area_yyyy.nc glacier_area_daily.nc
+cdo -yearadd -gtc,100000000 -inttime,2000-01-01,00:00:00,1day glacier_perc_per_gridcell.nc glacier_perc_per_gridcell.nc glacier_perc_daily.nc
 
 # then remove last day of data
-#cdo -delete,date="2100-01-01T00:00:00" glacier_area_daily.nc glacier_area_daily_v2.nc
-#cdo -delete,date="2100-01-01T00:00:00" glacier_perc_daily.nc glacier_perc_daily_v2.nc
+cdo -delete,date="2100-01-01T00:00:00" glacier_area_daily.nc glacier_area_daily_v2.nc
+cdo -delete,date="2100-01-01T00:00:00" glacier_perc_daily.nc glacier_perc_daily_v2.nc
 
 # also remove files not needed anymore
-#rm glacier_area_yyyy.nc
-#rm glacier_area_yyyy_v2.nc
-#rm glacier_perc_per_gridcell.nc
-#rm glacier_perc_per_gridcell_v2.nc
+rm glacier_area_yyyy.nc
+rm glacier_area_yyyy_v2.nc
+rm glacier_perc_per_gridcell.nc
+rm glacier_perc_per_gridcell_v2.nc
 
 cd $PBS_O_WORKDIR
 
-#python ${SRC_DIR}/coupling_pt2.py
+python ${SRC_DIR}/coupling_pt2.py
 
 cd $SCENARIO_SH
 # Delete files not needed
-#rm glacier_area_daily.nc
-#rm glacier_area_daily_v2.nc
-#rm glacier_perc_daily.nc
-#rm glacier_perc_daily_v2.nc
-#rm rain_daily_kgm2s.nc
+rm glacier_area_daily.nc
+rm glacier_area_daily_v2.nc
+rm glacier_perc_daily.nc
+rm glacier_perc_daily_v2.nc
+rm rain_daily_kgm2s.nc
 
 cd $PBS_O_WORKDIR
 echo "Resampling jules output"
-#./03c_resample_jules_output.sh $CONFIG_FILE
+./03c_resample_jules_output.sh $CONFIG_FILE
 echo "Resampling complete"
 echo "Computing jules annual mean runoff"
-#./04c_compute_jules_annual_mean_runoff.sh $CONFIG_FILE
+./04c_compute_jules_annual_mean_runoff.sh $CONFIG_FILE
 echo "Annual mean runoff complete"
 echo "Combining jules runoff"
-#./05c_combine_jules_runoff.sh $CONFIG_FILE
+./05c_combine_jules_runoff.sh $CONFIG_FILE
 echo "Combined jules runoff complete"
 echo "Create mosart input"
-#./07_create-mosartwm-input.sh $CONFIG_FILE
+./07_create-mosartwm-input.sh $CONFIG_FILE
 echo "Mosart input complete"
-#./08_scaling_and_partitioning.sh $CONFIG_FILE
+./08_scaling_and_partitioning.sh $CONFIG_FILE
 
 echo "Running Mosart"
 NUMBA_NUM_THREADS=32 ./$MOSART_FILE
